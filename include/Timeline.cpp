a0e6ebf1c7b68ffe80e2dd08a96c51a9b7d8eaee
Andrew Bell <cinder.afb@gmail.com>
2011-04-17 13:27:29 -0400

    Copyright messages

--- include/Timeline.cpp
+++ include/Timeline.cpp
@@ -1,20 +1,36 @@
 /*
- *  Timeline.cpp
- *  BasicTween
- *
- *  Created by David Wicks on 5/27/10.
- *  Copyright 2010 David Wicks. All rights reserved.
- *
- */
+ Copyright (c) 2011, The Cinder Project, All rights reserved.
+ This code is intended for use with the Cinder C++ library: http://libcinder.org
+
+ Based on the sc-Choreograph CinderBlock by David Wicks: http://sansumbrella.com/
+
+ Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+ the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice, this list of conditions and
+	the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+	the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+*/
 
 #include "Timeline.h"
 
 #include <vector>
 
 using namespace cinder;
-using namespace cinder::tween;
-typedef std::vector<TimelineItemRef>::iterator s_iter;
 using namespace std;
+using namespace cinder::tween;
+
+typedef std::vector<TimelineItemRef>::iterator s_iter;
 
 Timeline::Timeline()
 	: TimelineItem( 0, 0, 0, 0 ), mDefaultAutoRemove( true ), mCurrentTime( 0 )
