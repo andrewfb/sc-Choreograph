c2f6e14d26785785722a7b50ce9c6b6f947158c0
Chris McKenzie <chris.mckenzie@gmail.com>
2011-04-20 12:12:05 -0400

    Added Trey Ratcliff photos and improved accordion

--- samples/ImageAccordion/include/AccordionItem.h
+++ samples/ImageAccordion/include/AccordionItem.h
@@ -1,35 +1,33 @@
-/*
- *  AccordionItem.h
- *  ImageAccordion
- *
- *  Created by Chris McKenzie on 4/15/11.
- *  Copyright 2011 __MyCompanyName__. All rights reserved.
- *
- */
 
 #pragma once
 
 #include "cinder/Color.h"
 #include "cinder/Rect.h"
+#include "cinder/Area.h"
 #include "cinder/Vector.h"
+#include "cinder/gl/Texture.h"
 #include <vector>
 
 using namespace ci;
+using namespace gl;
 
 class AccordionItem {
 public:
 	
 	AccordionItem();
-	AccordionItem( float x, float y, float width, float height );
+	AccordionItem( float x, float y, float height, float contractedWidth, float expandedHeight, Texture image );
 	void update();
 	void draw();
+	bool isPointIn( const Vec2f &pt );
+	float round( float n );
 	
-	float mX, mY, mWidth, mHeight;
+	float mX, mY, mWidth, mHeight, mExpandedWidth;
 	
 	Rectf mRect;
 	Color mColor;
 	
-	bool isPointIn( const Vec2f &pt );
+	Texture mImage;
+	Area    mImageArea;
 	
 };
 
